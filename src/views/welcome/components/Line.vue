9ae9438df3b30fd562725a8f1f3d7bead465a5fd
Sommer Jiang <sommer.jiang78@gmail.com>
2022-07-05 11:29:25 +0800

    Initial commit

---
+++ src/views/welcome/components/Line.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+import { useECharts } from "@pureadmin/utils";
+
+const lineChartRef = ref<HTMLDivElement | null>(null);
+const { setOptions } = useECharts(lineChartRef as Ref<HTMLDivElement>);
+
+setOptions(
+  {
+    grid: {
+      bottom: "20%",
+      height: "68%",
+      containLabel: true
+    },
+    tooltip: {
+      trigger: "item"
+    },
+    xAxis: {
+      type: "category",
+      axisLabel: {
+        interval: 0
+      },
+      data: ["open_issues", "forks", "watchers", "star"]
+    },
+    yAxis: {
+      type: "value"
+    },
+    series: [
+      {
+        data: [3, 204, 1079, 1079],
+        type: "line",
+        areaStyle: {}
+      }
+    ]
+  },
+  {
+    name: "click",
+    callback: params => {
+      console.log("click", params);
+    }
+  }
+);
+</script>
+
+<template>
+  <div ref="lineChartRef" style="width: 100%; height: 35vh" />
+</template>
---
+++ src/views/welcome/components/Pie.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+import { useECharts } from "@pureadmin/utils";
+
+const pieChartRef = ref<HTMLDivElement | null>(null);
+const { setOptions } = useECharts(pieChartRef as Ref<HTMLDivElement>);
+
+setOptions(
+  {
+    tooltip: {
+      trigger: "item"
+    },
+    legend: {
+      orient: "vertical",
+      right: true
+    },
+    series: [
+      {
+        name: "Github信息",
+        type: "pie",
+        radius: "60%",
+        center: ["40%", "50%"],
+        data: [
+          { value: 1079, name: "watchers" },
+          { value: 1079, name: "star" },
+          { value: 204, name: "forks" },
+          { value: 3, name: "open_issues" }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)"
+          }
+        }
+      }
+    ]
+  },
+  {
+    name: "click",
+    callback: params => {
+      console.log("click", params);
+    }
+  },
+  // 点击空白处
+  {
+    type: "zrender",
+    name: "click",
+    callback: params => {
+      console.log("点击空白处", params);
+    }
+  }
+);
+</script>
+
+<template>
+  <div ref="pieChartRef" style="width: 100%; height: 35vh" />
+</template>
